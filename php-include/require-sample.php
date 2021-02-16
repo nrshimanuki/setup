c6087b7283d4190fe73a364f88bc4a2d772ef18e
nrshima <nrshima404@gmail.com>
2021-02-16 16:58:53 +0900

    edit

--- php-include/require-sample.php
+++ php-include/require-sample.php
@@ -1,4 +1,8 @@
-<?php // require_once( dirname( __FILE__ ) . '/wp/wp-blog-header.php' ); ?>
+<?php
+// WP専用ディレクトリ:
+// require_once( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+// パーマリンクを変更すると .htaccess が書き換えられてしまうので、パーミッションを読み取りのみに変更する
+?>
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'].'/assets/inc/__constant.php');
 	$page_slug        = INDEX_PAGE_SLUG;
