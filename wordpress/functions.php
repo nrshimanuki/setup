6f5c57767b8813f1fd84d560d97c973d2f12c72f
nrshima <nrshima404@gmail.com>
2021-04-01 00:05:52 +0900

    edit

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -394,27 +394,23 @@ add_filter('wpcf7_autop_or_not', '__return_false');
 /** ========================================
  * contactform7 メールアドレス再確認チェック
  */
-function wpcf7_main_validation_filter( $result, $tag ) {
+function wpcf7_validate_email_filter_confrim( $result, $tag ) {
 	$type = $tag['type'];
 	$name = $tag['name'];
-	$_POST[$name] = trim( strtr( (string) $_POST[$name], "\n", " " ) );
 	if ( 'email' == $type || 'email*' == $type ) {
-		if (preg_match('/(.*)_confirm$/', $name, $matches)){
+		if (preg_match('/(.*)_confirm$/', $name, $matches)){ //確認用メルアド入力フォーム名を ○○○_confirm としています。
 			$target_name = $matches[1];
-			if ($_POST[$name] != $_POST[$target_name]) {
-				if (method_exists($result, 'invalidate')) {
-					$result->invalidate( $tag,"確認用のメールアドレスが一致していません");
-				} else {
-					$result['valid'] = false;
-					$result['reason'][$name] = '確認用のメールアドレスが一致していません';
-				}
+				$posted_value = trim( (string) $_POST[$name] ); //前後空白の削除
+				$posted_target_value = trim( (string) $_POST[$target_name] ); //前後空白の削除
+			if ($posted_value != $posted_target_value) {
+				$result->invalidate( $tag,"確認用のメールアドレスが一致していません");
 			}
 		}
 	}
 	return $result;
 }
-add_filter( 'wpcf7_validate_email', 'wpcf7_main_validation_filter', 11, 2 );
-add_filter( 'wpcf7_validate_email*', 'wpcf7_main_validation_filter', 11, 2 );
+add_filter( 'wpcf7_validate_email', 'wpcf7_validate_email_filter_confrim', 11, 2 );
+add_filter( 'wpcf7_validate_email*', 'wpcf7_validate_email_filter_confrim', 11, 2 );
 
 
 
